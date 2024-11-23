1f72c9fb724a6f113d79bf6b9a03d40d496dce02
vs0uz4 <vitor.rodrigues@gmail.com>
2024-11-23 16:33:33 -0300

    start and running server

--- main.go
+++ main.go
@@ -13,13 +13,20 @@ func main() {
 		panic(err)
 	}
 
+	handlerRoot := func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Enjoy the silence!"))
+	}
+
 	handler := func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 	}
 
 	webserver := webserver.NewWebServer(cfg.WebServerPort)
 	webserver.AddHandler("/test", handler, "GET")
+	webserver.AddHandler("/", handlerRoot, "GET")
 
 	fmt.Println("Starting web server on port", cfg.WebServerPort)
 	webserver.Start()
+	webserver.Run()
 }
