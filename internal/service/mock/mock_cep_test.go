9ec783cae09a8e886139b584e876e0b5bed0ebd4
vs0uz4 <vitor.rodrigues@gmail.com>
2024-12-12 20:17:03 -0300

    add tests for mocks

---
+++ internal/service/mock/mock_cep_test.go
@@ -0,0 +1,27 @@
+package mock
+
+import (
+	"testing"
+	"weatherzip/internal/domain"
+)
+
+func TestMockCepService(t *testing.T) {
+	mock := MockCepService{
+		GetLocationFunc: func(cep string) (domain.CepResponse, error) {
+			if cep == "12345678" {
+				return domain.CepResponse{Cep: "12345678"}, nil
+			}
+			return domain.CepResponse{}, domain.ErrZipcodeNotFound
+		},
+	}
+
+	response, err := mock.GetLocation("12345678")
+	if response.Cep != "12345678" || err != nil {
+		t.Errorf("Expected Cep: 12345678, got: %v, err: %v", response.Cep, err)
+	}
+
+	_, err = mock.GetLocation("00000000")
+	if err != domain.ErrZipcodeNotFound {
+		t.Errorf("Expected error: %v, got: %v", domain.ErrZipcodeNotFound, err)
+	}
+}
---
+++ internal/service/mock/mock_weather_test.go
@@ -0,0 +1,27 @@
+package mock
+
+import (
+	"testing"
+	"weatherzip/internal/domain"
+)
+
+func TestMockWeatherService(t *testing.T) {
+	mock := MockWeatherService{
+		GetWeatherFunc: func(location string) (domain.WeatherResponse, error) {
+			if location == "Valid Location" {
+				return domain.WeatherResponse{Current: domain.CurrentWeather{TempC: 25.0}}, nil
+			}
+			return domain.WeatherResponse{}, domain.ErrUnexpectedBadRequest
+		},
+	}
+
+	response, err := mock.GetWeather("Valid Location")
+	if response.Current.TempC != 25.0 || err != nil {
+		t.Errorf("Expected TempC: 25.0, got: %v, err: %v", response.Current.TempC, err)
+	}
+
+	_, err = mock.GetWeather("Invalid Location")
+	if err != domain.ErrUnexpectedBadRequest {
+		t.Errorf("Expected error: %v, got: %v", domain.ErrUnexpectedBadRequest, err)
+	}
+}
