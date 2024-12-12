8f85b639d719f2fc2eb52999f0170795e4d93b4a
vs0uz4 <vitor.rodrigues@gmail.com>
2024-12-12 15:45:50 -0300

    request decoding error scenario coverage

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,7 +4,10 @@ import (
 	"fmt"
 	"net/http"
 	"weatherzip/configs"
+	"weatherzip/internal/infra/web"
 	"weatherzip/internal/infra/web/webserver"
+	"weatherzip/internal/service"
+	"weatherzip/internal/usecase"
 )
 
 func main() {
@@ -13,6 +16,17 @@ func main() {
 		panic(err)
 	}
 
+	httpClient := &http.Client{}
+
+	cpuService := service.NewCPUService()
+	memoryService := service.NewMemoryService()
+	uptimeService := service.NewUptimeService()
+	cepService := service.NewCepService(httpClient, cfg.CepAPIUrl)
+	weatherService := service.NewWeatherService(httpClient, cfg.WeatherAPIUrl, cfg.WeatherAPIKey, cfg.WeatherAPILanguage)
+
+	healthCheckUseCase := usecase.NewHealthCheckUseCase(cpuService, memoryService, uptimeService)
+	wheaterByCepUseCase := usecase.NewWeatherByCepUsecase(cepService, weatherService)
+
 	handlerRoot := func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		if _, err := w.Write([]byte("Enjoy the silence!")); err != nil {
@@ -20,12 +34,12 @@ func main() {
 		}
 	}
 
-	handler := func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-	}
+	handlerHealth := web.NewHealthHandler(healthCheckUseCase).GetHealth
+	handlerWeather := web.NewWeatherHandler(wheaterByCepUseCase).GetWeatherByCep
 
 	webserver := webserver.NewWebServer(cfg.WebServerPort)
-	webserver.AddHandler("/test", handler, "GET")
+	webserver.AddHandler("/weather/{cep}", handlerWeather, "GET")
+	webserver.AddHandler("/health", handlerHealth, "GET")
 	webserver.AddHandler("/", handlerRoot, "GET")
 
 	fmt.Println("Starting web server on port", cfg.WebServerPort)
